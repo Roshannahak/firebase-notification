2216ecbd095c62777c6703afa274fef29087400a
roshan nahak <roshannahak1122@gmail.com>
2021-09-19 01:26:02 +0530

    [done]

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+const AndroidNotificationChannel channel =
+    AndroidNotificationChannel("id", "name", "description");
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage msg) async {
+  await Firebase.initializeApp();
+  print("background message: ${msg.messageId}");
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
+
   runApp(MaterialApp(
     title: "FCM test app",
     debugShowCheckedModeBanner: false,
@@ -16,6 +42,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreen extends State<HomeScreen> {
+  @override
+  void initState() {
+    getDeviceToken();
+    FirebaseMessaging.onMessage.listen((RemoteMessage msg) {
+      RemoteNotification? notification = msg.notification;
+      AndroidNotification? androidNotification = msg.notification?.android;
+      if (notification != null && androidNotification != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(
+                    channel.id, channel.name, channel.description,
+                    playSound: true, icon: '@mipmap/ic_launcher')));
+      }
+    });
+    super.initState();
+  }
+
+  getDeviceToken() async {
+    var token = await FirebaseMessaging.instance.getToken();
+    print(
+        token); //fv3x5X6cQumXHQTF-rtoov:APA91bHor6IuKuypALlzutOhr1VzODQR5PsAsQf6O_BFSARomN0w-H4EggKousu53Rfn4X_wpXGQ4m2Z5JmAuSqyfRaRiySrzOgwKLNaR8eXfdsLp4qaH_zJ0F_Esylti2zzTVe_HOS9
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
